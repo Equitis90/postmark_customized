587e62948309f84bc750af1ca46093dca3976173
Vladyslav Sydorenko <svuitis@gmail.com>
2020-04-08 09:36:11 +0300

    fix: add PostmarkCustomized module

--- lib/griddler/postmark_customized.rb
+++ lib/griddler/postmark_customized.rb
@@ -2,4 +2,9 @@ require "griddler"
 require "griddler/postmark_customized/version"
 require "griddler/postmark_customized/adapter"
 
+module Griddler
+  module PostmarkCustomized
+  end
+end
+
 Griddler.adapter_registry.register(:postmark_customized, Griddler::PostmarkCustomized::Adapter)
--- lib/griddler/postmark_customized/version.rb
+++ lib/griddler/postmark_customized/version.rb
@@ -1,5 +1,5 @@
 module Griddler
   module PostmarkCustomized
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
   end
 end
